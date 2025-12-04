6e5221ca3c7015a1c58c7c9890398cd13b087190
netlogyc <63619346+netlogyc@users.noreply.github.com>
2025-12-04 18:37:42 -0500

    Usar rutas relativas en dashboard (./ y ./bots) para funcionar bajo /chatbotnl/public

--- views/dashboard.php
+++ views/dashboard.php
@@ -9,10 +9,10 @@
 <body class="bg-light">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/">Connectbot</a>
+        <a class="navbar-brand" href="./">Connectbot</a>
         <div class="collapse navbar-collapse">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item"><a class="nav-link" href="/bots">Bots</a></li>
+                <li class="nav-item"><a class="nav-link" href="./bots">Bots</a></li>
             </ul>
         </div>
     </div>
@@ -23,7 +23,7 @@
         <div class="col-12">
             <h1 class="mb-3">Panel de Connectbot</h1>
             <p class="lead">Desde aquí vas a gestionar bots, fuentes (URLs) y conexiones con n8n y Evolution API.</p>
-            <a href="/bots" class="btn btn-primary">Gestionar bots</a>
+            <a href="./bots" class="btn btn-primary">Gestionar bots</a>
         </div>
     </div>
 </div>
